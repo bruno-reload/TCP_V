3e64d48c627f469356786731bf65b9c787921e48
Alexandregaudencio <alexandre.gtpjr@gmail.com>
2022-05-20 17:49:38 -0300

    character scripts: behaviour; properties

---
+++ Assets/Game/Script/Character/CharacterProperties.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Character
+{
+    [CreateAssetMenu(fileName = "Character properties")]
+    public class CharacterProperties : ScriptableObject
+    {
+        [SerializeField] private float speed;
+        [SerializeField] private float jumpImpulse;
+        [SerializeField] private float headForce;
+
+        public float Speed { get => speed; set => speed = value; }
+        public float JumpImpulse { get => jumpImpulse; set => jumpImpulse = value; }
+        public float HeadForce { get => headForce; set => headForce = value; }
+    }
+}
+
